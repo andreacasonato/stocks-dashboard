a2153cda1fdaf96fccd8a8f908c85961c7c18bec
Andrea <134276451+andreacasonato@users.noreply.github.com>
2025-12-27 12:48:55 +0100

    Add DOM elements

--- script.js
+++ script.js
@@ -2,3 +2,8 @@
 
 const API_KEY = "B296NQOAOTD70U3Y";
 let myChart = null;
+
+// ----- DOM ELEMENTS -----
+
+const stockInput = document.getElementById("stockInput");
+const searchBtn = document.getElementById("searchBtn");
